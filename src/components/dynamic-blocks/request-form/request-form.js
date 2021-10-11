41d1dc6a017e872605545ea7f93ff797ba8a178c
Vladimir Miasin <looser.s.usami@gmail.com>
2021-10-12 00:25:21 +0300

    Перевел формы на Formik

--- src/components/dynamic-blocks/request-form/request-form.js
+++ src/components/dynamic-blocks/request-form/request-form.js
@@ -1,109 +1,147 @@
-import React, {Component} from 'react';
+// import React, {Component} from 'react';
+// import {useFormik} from 'formik';
 import './style.css';
 
-export default class RequestForm extends Component {
+// export default function RequestForm () {
 
-    DATA;
-    
-    constructor(props){
-        super(props);
-        this.eventName = this.eventName.bind(this);
-        this.eventSecondname = this.eventSecondname.bind(this);
-        this.eventPhoneNumber = this.eventPhoneNumber.bind(this);
-        this.eventTG = this.eventTG.bind(this);
-        this.toggleLayout = this.toggleLayout.bind(this);
-        this.onFormSubmit = this.onFormSubmit.bind(this);
-        this.state={
+//     const validate = values => {
+//         const errors = {}
+
+
+//     }
+
+//     const formik = useFormik({
+//         initialValues:{
+//             name: "",
+            // secondname: "",
+            // phonenumber: "",
+            // tg: "",
+//             layout: true
+//         },
+//         onSubmit: values => {
+//             alert(JSON.stringify(values, null, 2))
+//         }
+//     })
+
+//     return(
+//         <div className="request">
+//             <form onSubmit={formik.handleSubmit}>
+//                 <fieldset className="inputs">
+//                     <input type="text" placeholder="Имя" id="name"
+//                         onChange={formik.handleChange}
+//                         value={formik.values.name} />
+//                     <input type="text" placeholder="Фамилия" id="secondname"
+//                         onChange={formik.handleChange}
+//                         value={formik.values.secondname} />
+//                     <input type="text" placeholder="Номер телефона" id="phonenumber"
+//                         onChange={formik.handleChange}
+//                         value={formik.values.phonenumber} />
+//                     <input type="text" placeholder="Ник в Telegram" id="tg"
+//                         onChange={formik.handleChange}
+//                         value={formik.values.tg} />
+//                 </fieldset>
+//                 <fieldset>
+//                     <h3>Какое направление Вам интересно?</h3>
+//                     <input type="checkbox" id="layout" name="direction" value="layout"
+//                         defaultChecked={formik.values.layout} />
+//                     <label htmlFor="layout"
+//                         onPress={() => setFieldValue('layout', !formik.values.layout)}
+//                         >Верстка</label>
+//                     <input type="checkbox" id="frontend" name="direction" value="frontend"/>
+//                     <label htmlFor="frontend">Front-end</label>
+//                     <input type="checkbox" id="backend" name="direction" value="backend"/>
+//                     <label htmlFor="backend">Backend</label>
+//                 </fieldset>
+//                 <fieldset>
+//                     <button type="submit">Оставить заявку</button>
+//                     <p>Нажимая “Оставить заявку” Вы соглашаетесь<br />с обработкой персональных данных</p>
+//                 </fieldset>
+//             </form>
+//         </div>
+//     );
+// }
+
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Formik, Field, Form } from 'formik';
+import './style.css';
+
+const sleep = (ms) => new Promise((r) => setTimeout(r, ms));
+
+const validate = values =>{
+    const errors = {}
+
+    if(!values.name){
+        errors.name = 'Обязательное поле'
+    }
+
+    return JSON.stringify(errors, null, 2)
+}
+
+const RequestForm = () => (
+  <div className="request">
+    <Formik
+        initialValues={{
             name: '',
             secondname: '',
-            phoneNumber: '',
+            phonenumber: '',
             tg: '',
-            layout: true,
-            frontend: false,
-            backend: false
-        };
-    }
+            directions: [],
+        }}
+        onSubmit={async (values, {resetForm}) => {
+            await sleep(500);
+            (validate(values) !== JSON.stringify({})) ? alert(validate(values)) : alert(JSON.stringify(values, null, 2));
+            // alert(JSON.stringify(values, null, 2));
+            resetForm()
+        }}
+    >
+    {({ values, handleChange, handleBlur }) => (
+        <Form>
+            <fieldset className="inputs">
+                <input type="text" placeholder="Имя" name="name"
+                    value={values.name}
+                    onChange={handleChange}
+                    onBlur={handleBlur} />
+                <input type="text" placeholder="Фамилия" name="secondname"
+                    value={values.secondname}
+                    onChange={handleChange}
+                    onBlur={handleBlur} />
+                <input type="text" placeholder="Номер телефона" name="phonenumber"
+                    value={values.phonenumber} 
+                    onChange={handleChange}
+                    onBlur={handleBlur} />
+                <input type="text" placeholder="Ник в Telegram" name="tg"
+                    value={values.tg}
+                    onChange={handleChange}
+                    onBlur={handleBlur} />
+            </fieldset>
+            <fieldset>
+                <h3>Какое направление Вам интересно?</h3>
+                <label className={values.directions.includes("layout") ? "cb checked" : "cb"} >
+                    <Field type="checkbox" name="directions" value="layout" />
+                    Верстка
+                </label>
+                <label className={values.directions.includes("frontend") ? "cb checked" : "cb"} >
+                    <Field type="checkbox" name="directions" value="frontend" />
+                    Front-end
+                </label>
+                <label className={values.directions.includes("backend") ? "cb checked" : "cb"} >
+                    <Field type="checkbox" name="directions" value="backend" />
+                    Backend
+                </label>
+            </fieldset>
+            <fieldset>
+                <button type="submit">Оставить заявку</button>
+                <p>Нажимая “Оставить заявку” Вы соглашаетесь
+                <br />с обработкой персональных данных</p>
+                <br />
+                <p></p>
+            </fieldset>
+            </Form>
+        )}
+        </Formik>
+    </div>
+    );
 
-    eventName(event) {
-        this.setState({ name: event.target.value })
-    }
-    eventSecondname(event) {
-        this.setState({ secondname: event.target.value })
-    }
-    eventPhoneNumber(event) {
-        this.setState({ phoneNumber: event.target.value })
-    }
-    eventTG(event) {
-        this.setState({ tg: event.target.value })
-    }
-    toggleLayout = () => {
-        this.setState({layout: !this.state.layout});
-    }
-    toggleFrontend = () => {
-        this.setState({frontend: !this.state.frontend});
-    }
-    toggleBackend = () => {
-        this.setState({backend: !this.state.backend});
-    }
-    onFormSubmit(event) {
-        event.preventDefault();
-        localStorage.setItem('contact_form', JSON.stringify(this.state));
-    }
-
-    componentDidMount() {
-        this.DATA = JSON.parse(localStorage.getItem('contact_form'));
-
-        if (localStorage.getItem('contact_form')) {
-            this.setState({
-                name: this.DATA.name,
-                secondname: this.DATA.secondname,
-                phoneNumber: this.DATA.phoneNumber,
-                tg: this.DATA.tg,
-                layout: this.DATA.layout,
-                frontend: this.DATA.frontend,
-                backend: this.DATA.backend
-            })
-        } else {
-            this.setState({
-                name: '',
-                secondname: '',
-                phoneNumber: '',
-                tg: '',
-                layout: true,
-                frontend: false,
-                backend: false
-            })
-        }
-    }
-
-    render(){
-        return(
-            <div className="request">
-                <form onSubmit={this.onFormSubmit}>
-                    <fieldset className="inputs">
-                        <input type="text" placeholder="Имя" value={this.state.name} onChange={this.eventName} />
-                        <input type="text" placeholder="Фамилия"  value={this.state.secondname} onChange={this.eventSecondname} />
-                        <input type="text" placeholder="Номер телефона" value={this.state.phoneNumber} onChange={this.eventPhoneNumber} />
-                        <input type="text" placeholder="Ник в Telegram" value={this.state.tg} onChange={this.eventTG} />
-                    </fieldset>
-                    <fieldset>
-                        <h3>Какое направление Вам интересно?</h3>
-                        <input type="checkbox" id="layout" name="direction" value="layout" defaultChecked={this.state.layout}/>
-                        <label htmlFor="layout" onClick={this.toggleLayout}
-                            >Верстка</label>
-                        <input type="checkbox" id="frontend" name="direction" value="frontend"
-                            defaultChecked={this.state.frontend} />
-                        <label htmlFor="frontend" onClick={this.toggleFrontend}>Front-end</label>
-                        <input type="checkbox" id="backend" name="direction" value="backend"
-                            defaultChecked={this.state.backend} />
-                        <label htmlFor="backend" onClick={this.toggleBackend}>Backend</label>
-                    </fieldset>
-                    <fieldset>
-                        <button type="submit">Оставить заявку</button>
-                        <p>Нажимая “Оставить заявку” Вы соглашаетесь<br />с обработкой персональных данных</p>
-                    </fieldset>
-                </form>
-            </div>
-        );
-    }
-}
+    export default RequestForm
